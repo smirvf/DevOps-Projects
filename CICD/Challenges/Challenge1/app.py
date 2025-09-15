fc48cc9f089e0c4cfb4c4b1c456dfe0b88c58255
saahir.mir <saahir.mir@vodafone.com>
2025-09-15 20:41:40 +0100

    Updating Python file

--- CICD/Challenges/Challenge1/app.py
+++ CICD/Challenges/Challenge1/app.py
@@ -5,19 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return f'Hello, World!'
-
-@app.route('/count')
-def counter():
-    # Check if counter exists, create if it does not exist
-    if db.exists('counter') == False:
-        db.set ('counter', 1)
-    else:
-        db.incr('counter', 1)
-
-    count=db.get('counter')
-
-    return f'Hello, World! This site has had {count} visits.'
+    return f'Hello, World! This is a placeholder text to test my CICD Code'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002)
