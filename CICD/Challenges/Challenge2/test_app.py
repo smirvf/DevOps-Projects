56b7e7b0e05a298cf351b2bfc39284f7657e9e20
saahir.mir <saahir.mir@vodafone.com>
2025-09-16 19:46:01 +0100

    correcting test app.py

--- CICD/Challenges/Challenge2/test_app.py
+++ CICD/Challenges/Challenge2/test_app.py
@@ -4,4 +4,4 @@ from appT import hello_world
 
 class TestAppT(unittest.TestCase):
     def test_say_hello(self):
-        self.assertEqual(hello_world(), "")
+        self.assertEqual(hello_world(), "Hello, World! This is a placeholder text to test my CICD Code")
